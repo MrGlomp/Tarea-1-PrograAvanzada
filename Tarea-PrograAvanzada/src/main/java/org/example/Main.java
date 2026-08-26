ed8b41a4ab4b7d4f2eb312dbde4919b7b73ec680
MrGlomp <e.concha04@ufromail.cl>
2026-08-26 12:53:51 -0400

    .

--- Tarea-PrograAvanzada/src/main/java/org/example/Main.java
+++ Tarea-PrograAvanzada/src/main/java/org/example/Main.java
@@ -3,5 +3,16 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        int num = 3;
+        System.out.println("El factorial de "+num+" es: "+factorial(num));
+
+    }
+
+    public static int factorial(int n) {
+        int resultado = 1;
+        for (int i = 1; i <= n; i++) {
+            resultado *= i;
+        }
+        return resultado;
     }
 }

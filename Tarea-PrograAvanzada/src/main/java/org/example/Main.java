751809b823eb5a2425956183af88ba139eb9ed1d
MrGlomp <e.concha04@ufromail.cl>
2026-08-26 16:28:05 -0400

    .

--- Tarea-PrograAvanzada/src/main/java/org/example/Main.java
+++ Tarea-PrograAvanzada/src/main/java/org/example/Main.java
@@ -1,18 +1,44 @@
 package org.example;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        int num = 3;
-        System.out.println("El factorial de "+num+" es: "+factorial(num));
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.println("Ingrese el un numero: ");
+        num = sc.nextInt();
+        long tiempoInicio = System.nanoTime();
+        System.out.println("El factorial de "+num+" es: "+factorialRecursivo(num));
+        long tiempoFin = System.nanoTime();
+        double duracionSegundos = (tiempoFin - tiempoInicio) / 1_000_000_000.0;
+        System.out.println("Tiempo demorado: "+duracionSegundos+" segundos");
 
     }
 
-    public static int factorial(int n) {
-        int resultado = 1;
+    public static BigInteger factorial(int n) {
+
+        if (n < 0) {
+            throw new IllegalArgumentException("El numero debe ser positivo");
+        }
+
+        BigInteger resultado = BigInteger.ONE;
         for (int i = 1; i <= n; i++) {
-            resultado *= i;
+            resultado = resultado.multiply(BigInteger.valueOf(i));
         }
         return resultado;
+    }
+
+    public static BigInteger factorialRecursivo(int n) {
+
+        if (n < 0) {
+            throw new IllegalArgumentException("El numero debe ser positivo");
+        }
+
+        if (n == 0 || n == 1) {
+            return BigInteger.ONE;
+        }
+        return BigInteger.valueOf(n).multiply(factorialRecursivo(n - 1));
     }
 }
